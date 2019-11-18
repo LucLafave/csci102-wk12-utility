ca2351778f855c6373585af4a85ff86d3819545e
LucLafave <luclafave@mymail.mines.edu>
2019-11-18 09:37:11 -0700

    Finished second function LoadFile

--- Week12-utility.py
+++ Week12-utility.py
@@ -5,3 +5,9 @@
 
 def PrintOutput(input_string):
     print('OUTPUT %s' % (input_string))
+def LoadFile(file_name):
+    with open(file_name) as file_in:
+        file_list = file_in.readlines()
+        for i in range(len(file_list)):
+            file_list[i] = file_list[i][0:len(file_list[i])-1]
+        return file_list
